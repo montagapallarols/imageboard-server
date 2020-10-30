39e70b102290d9a06664916e60cb4229692505c0
montipallarols <montaga.pallarols@gmail.com>
2020-10-30 10:45:39 +0100

    Add pagination routes

--- routers/imageRouter.js
+++ routers/imageRouter.js
@@ -4,10 +4,49 @@ const Image = require("../models").image;
 const router = new Router();
 
 
-router.get("/", async (req, res, next) => {
-    const images = await Image.findAll()
-    res.json(images)
+// router.get("/", async (req, res, next) => {
+//     const images = await Image.findAll()
+//     res.json(images)
+// })
+
+router.get("/:imageId", async (req, res, next) => {
+    try {
+        const id = parseInt(req.params.imageId)
+        const image = await Image.findByPk(id)
+        if (!image) {
+            res.status(404).send("Image not found")
+        } else {
+            res.json(image)
+        }
+    } catch(e) {
+        next(e)
+    }
 })
+
+
+// router.get("/", (req, res, next) => {
+//     const limit = req.query.limit || 3;
+//     const offset = req.query.offset || 0;
+  
+//     Image.findAll({
+//       limit,
+//       offset,
+//     })
+//       .then((images) => {
+//         res.send(images);
+//       })
+//       .catch((error) => next(error));
+//   });
+
+  router.get("/", (req, res, next) => {
+    const limit = req.query.limit || 25;
+    const offset = req.query.offset || 0;
+  
+    Image.findAndCountAll({ limit, offset })
+      .then((result) => res.send({ images: result.rows, total: result.count }))
+      .catch((error) => next(error));
+  });
+
 
 router.post("/", async (req, res, next) => {
     try {
