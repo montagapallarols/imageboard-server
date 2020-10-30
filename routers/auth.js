161392d44ffe863795a0638cccfa0a7a3d7d5c0a
montipallarols <montaga.pallarols@gmail.com>
2020-10-30 12:34:36 +0100

    Add signup endpoint

--- routers/auth.js
+++ routers/auth.js
@@ -4,6 +4,26 @@ const bcrypt = require('bcrypt');
 const User = require("../models").user;
 const router = new Router()
 
+
+router.post("/signup", async (req, res, next) => {
+    const { email, password, fullName } = req.body;
+if (!email || !password || !fullName) {
+  res.status(400).send("missing parameters");
+} else {
+  const newUser = await User.create({
+    email,
+    // Here, when handing down the password to the create method we hash it.
+    password: bcrypt.hashSync(password, 10),
+    fullName,
+  });
+
+  res.json(newUser);
+}
+})
+
+
+
+
 router.post('/login', async (req, res, next) => {
     const { email , password} = req.body
     if(!email || !password){
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,5 +1,6 @@
 const { Router } = require("express");
 const User = require("../models").user;
+const bcrypt = require('bcrypt');
 
 const router = new Router();
 
