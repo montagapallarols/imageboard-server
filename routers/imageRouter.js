2b52a9aeab3a0bf33ee6ceb2fbe320d2d808572e
montipallarols <montaga.pallarols@gmail.com>
2020-10-30 12:55:08 +0100

    Add Bearer header protection to get/images

--- routers/imageRouter.js
+++ routers/imageRouter.js
@@ -1,6 +1,6 @@
 const { Router } = require("express");
 const Image = require("../models").image;
-
+const { toJWT, toData } = require("../auth/jwt");
 const router = new Router();
 
 
@@ -38,13 +38,32 @@ router.get("/:imageId", async (req, res, next) => {
 //       .catch((error) => next(error));
 //   });
 
-  router.get("/", (req, res, next) => {
-    const limit = Math.min(req.query.limit || 25, 500);
-    const offset = req.query.offset || 0;
   
-    Image.findAndCountAll({ limit, offset })
-      .then((result) => res.send({ images: result.rows, total: result.count }))
-      .catch((error) => next(error));
+// router.get("/", (req, res, next) => {
+//     const limit = Math.min(req.query.limit || 25, 500);
+//     const offset = req.query.offset || 0;
+  
+//     Image.findAndCountAll({ limit, offset })
+//       .then((result) => res.send({ images: result.rows, total: result.count }))
+//       .catch((error) => next(error));
+// });
+
+
+  router.get("/", async (req, res, next) => {
+    const auth = req.headers.authorization && req.headers.authorization.split(" ");
+    if (auth && auth[0] === "Bearer" && auth[1]) {
+      try {
+        const data = toData(auth[1]);
+      } catch (e) {
+        res.status(400).send("Invalid JWT token");
+      }
+      const allImages = await Image.findAll();
+      res.json(allImages);
+    } else {
+      res.status(401).send({
+        message: "Please supply some valid credentials",
+      });
+    }
   });
 
 
